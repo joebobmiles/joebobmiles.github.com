2d95310dbf80f4c4080c5be1672190f620e4d311
Joseph R Miles <me@josephrmiles.com>
2020-04-07 08:47:15 -0700

    Implemented test for index.tsx

---
+++ src/tests/Site.spec.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { shallow } from "enzyme"
+
+import Site from "../pages/index"
+
+describe("Home page", () => {
+
+    it("Renders without error.", () => {
+        expect(shallow(<Site />)).toExist()
+    })
+
+})
