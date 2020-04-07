b11faf07029106cd63362fe5bc53f1beca0ff934
Joseph R Miles <me@josephrmiles.com>
2020-04-07 13:45:53 -0700

    Finished Layout component tests

--- src/tests/Layout.spec.tsx
+++ src/tests/Layout.spec.tsx
@@ -6,17 +6,50 @@ import Layout from "../components/Layout"
 
 describe("Layout component", () => {
 
-    const layoutWrapper = shallow(<Layout></Layout>)
+    describe("When shallow rendered without any children.", () => {
 
-    it("Renders without error.", () => {
-        expect(layoutWrapper).toHaveLength(1)
+        const layoutWrapper = shallow(<Layout></Layout>)
+
+        it("Renders without error.", () => {
+            expect(layoutWrapper).toHaveLength(1)
+        })
+
+        it("Has the website name in the heading", () => {
+            expect(layoutWrapper.find("h1").text()).toEqual("Notes2Self")
+        })
+
+        it ("Has the copyright in the footer", () => {
+            expect(layoutWrapper.find("p").text()).toEqual("© Joseph R Miles 2020")
+        })
+
     })
 
-    it("Has the website name in the heading", () => {
-        expect(layoutWrapper.find("h1").text()).toEqual("Notes2Self")
+    describe("When shallow rendered with children", () => {
+        
+        const message = "Hello World!"
+
+        const layoutWrapper = shallow(
+            <Layout>
+                <p>{message}</p>
+            </Layout>
+        )
+
+        it("Renders without errors", () => {
+            expect(layoutWrapper).toHaveLength(1)
+        })
+
+        it("Has the website name in the heading", () => {
+            expect(layoutWrapper.find("h1").text()).toEqual("Notes2Self")
+        })
+
+        it("Renders the given children", () => {
+            expect(layoutWrapper.find("p").first().text()).toEqual(message)
+        })
+
+        it ("Has the copyright in the footer", () => {
+            expect(layoutWrapper.find("p").last().text()).toEqual("© Joseph R Miles 2020")
+        })
+
     })
 
-    it ("Has the copyright in the footer", () => {
-        expect(layoutWrapper.find("p").text()).toEqual("© Joseph R Miles 2020")
-    })
 })
