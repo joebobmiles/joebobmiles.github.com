1f82721c53ff43e2b8cce855afbbd85272f706e1
Joseph R Miles <me@josephrmiles.com>
2020-04-07 18:08:29 -0700

    Connected articles to index page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,12 @@
 import React from "react"
 
 
-type LayoutProps = {
-    children?: any
-}
+export default ({ children }) => (
+    <>
+        <h1>Notes2Self</h1>
 
+        {children}
 
-export default ({ children }: LayoutProps) => (
-  <>
-    <h1>Notes2Self</h1>
-
-    {children}
-
-    <p>&copy; Joseph R Miles 2020</p>
-  </>
+        <p>&copy; Joseph R Miles 2020</p>
+    </>
 )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,54 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/Layout"
 
-const Site = () => (
-  <Layout>
-    <p>No shit notes, how-to's, pointers, tips, and tricks.</p>
-  </Layout>
-)
+
+const Site = ({ data }) => {
+
+    const allMdx = data.allMdx
+
+    const { totalCount, edges } = allMdx
+
+    return (
+        <Layout>
+            {edges.map(({ node }) => node)
+            .map(({ frontmatter, fields, excerpt, id }) => (
+                <Link key={id} to={fields.slug}>
+                    <article>
+                        <h3>
+                            {frontmatter.title}
+                            &mdash;
+                            {frontmatter.date}
+                        </h3>
+                        <p>{excerpt}</p>
+                    </article>
+                </Link>
+            ))}
+        </Layout>
+    )
+}
 
 export default Site
+
+
+export const query = graphql`
+query {
+    allMdx {
+        edges {
+            node {
+                frontmatter {
+                    date(formatString: "DD MMMM, YYYY")
+                    title
+                }
+                fields {
+                    slug
+                }
+                excerpt
+                id
+            }
+        }
+        totalCount
+    } 
+}
+`
