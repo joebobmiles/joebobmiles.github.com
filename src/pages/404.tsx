cad351b496e309168d07bc68be2df037602a8422
Joseph R Miles <joe@jrm.dev>
2021-01-24 10:54:10 -0800

    style: updated 404 page style

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,22 @@
 import React from "react"
+import classnames from "classnames";
+
+import Layout from "../components/Layout"
+import { left, right } from "../components/Layout.module.scss";
+import styles from "./404.module.scss";
 
 const NotFoundPage = () => (
-  <h1>404 - That's an error!</h1>
+  <Layout>
+    <h1 className={classnames(left, styles.text3X, styles.fontSemibold)}>
+      🤖 404
+    </h1>
+
+    <br/>
+
+    <p className={classnames(right, styles.textLg)}>
+      That's an error! 😱
+    </p>
+  </Layout>
 )
 
 export default NotFoundPage
