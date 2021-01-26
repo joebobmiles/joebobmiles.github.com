2935b03f5e138f8f28200eaa3996404582555a8f
Joseph R Miles <joe@jrm.dev>
2021-01-25 19:56:23 -0800

    fix: configured social media links to be external

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -63,46 +63,81 @@ export default ({ children }) =>
 
         <footer className={styles.footer}>
           <div>
-            <p><i className="far fa-copyright" title="&copy;"></i> 2020 &ndash; {currentYear} <Link to="/">{title}</Link></p>
             <nav className={styles.socialMediaLinks} aria-label="Social media quick links">
               <ul>
                 <li>
-                  <a href="mailto:joe@jrm.dev" title="Email">
+                  <a
+                    href="mailto:joe@jrm.dev"
+                    title="Email"
+                    target="_blank"
+                    rel="external noreferrer noopener nofollow"
+                  >
                     <i className="fa fa-at"></i>
                   </a>
                 </li>
                 <li>
-                  <a href="https://twitter.com/@joebobmiles" title="Twitter">
+                  <a
+                    href="https://twitter.com/@joebobmiles"
+                    title="Twitter"
+                    target="_blank"
+                    rel="external noreferrer noopener nofollow"
+                  >
                     <i className="fab fa-twitter"></i>
                   </a>
                 </li>
                 <li>
-                  <a href="https://github.com/joebobmiles" title="GitHub">
+                  <a
+                    href="https://github.com/joebobmiles"
+                    title="GitHub"
+                    target="_blank"
+                    rel="external noreferrer noopener nofollow"
+                  >
                     <i className="fab fa-github"></i>
                   </a>
                 </li>
                 <li>
-                  <a href="https://codepen.io/joebobmiles" title="CodePen">
+                  <a
+                    href="https://codepen.io/joebobmiles"
+                    title="CodePen"
+                    target="_blank"
+                    rel="external noreferrer noopener nofollow"
+                  >
                     <i className="fab fa-codepen"></i>
                   </a>
                 </li>
                 <li>
-                  <a href="https://dev.to/joebobmiles" title="DEV Community">
+                  <a
+                    href="https://dev.to/joebobmiles"
+                    title="DEV Community"
+                    target="_blank"
+                    rel="external noreferrer noopener nofollow"
+                  >
                     <i className="fab fa-dev"></i>
                   </a>
                 </li>
                 <li>
-                  <a href="https://medium.com/joebobmiles" title="Medium">
+                  <a
+                    href="https://medium.com/joebobmiles"
+                    title="Medium"
+                    target="_blank"
+                    rel="external noreferrer noopener nofollow"
+                  >
                     <i className="fab fa-medium"></i>
                   </a>
                 </li>
                 <li>
-                  <a href="https://linkedin.com/in/josephrmiles" title="LinkedIn">
+                  <a
+                    href="https://linkedin.com/in/josephrmiles"
+                    title="LinkedIn"
+                    target="_blank"
+                    rel="external noreferrer noopener nofollow"
+                  >
                     <i className="fab fa-linkedin"></i>
                   </a>
                 </li>
               </ul>
             </nav>
+            <p className={styles.textSm}><i className="far fa-copyright" title="&copy;"></i> 2020 &ndash; {currentYear} <Link to="/">{title}</Link></p>
           </div>
         </footer>
       </main>
