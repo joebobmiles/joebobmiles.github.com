c993a311d8f147d7beace1e3eb0cdcedae863a58
Joseph R Miles <joe@jrm.dev>
2021-01-25 19:50:12 -0800

    feat: added social media icons to layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -46,6 +46,8 @@ export default ({ children }) =>
 
         <meta name="author" content={author.name} />
         <meta name="description" content={description} />
+
+        <script src="https://kit.fontawesome.com/62cb22b716.js" crossOrigin="anonymous"></script>
       </Helmet>
 
       <main className={styles.container}>
@@ -60,7 +62,48 @@ export default ({ children }) =>
         </main>
 
         <footer className={styles.footer}>
-          <p>&copy; 2020 &ndash; {currentYear} <Link to="/">{title}</Link></p>
+          <div>
+            <p><i className="far fa-copyright" title="&copy;"></i> 2020 &ndash; {currentYear} <Link to="/">{title}</Link></p>
+            <nav className={styles.socialMediaLinks} aria-label="Social media quick links">
+              <ul>
+                <li>
+                  <a href="mailto:joe@jrm.dev" title="Email">
+                    <i className="fa fa-at"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://twitter.com/@joebobmiles" title="Twitter">
+                    <i className="fab fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://github.com/joebobmiles" title="GitHub">
+                    <i className="fab fa-github"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://codepen.io/joebobmiles" title="CodePen">
+                    <i className="fab fa-codepen"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://dev.to/joebobmiles" title="DEV Community">
+                    <i className="fab fa-dev"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://medium.com/joebobmiles" title="Medium">
+                    <i className="fab fa-medium"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://linkedin.com/in/josephrmiles" title="LinkedIn">
+                    <i className="fab fa-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </footer>
       </main>
     </>
